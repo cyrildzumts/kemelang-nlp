ec7a393277505dcc85da5839a007ce4012df113b
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2026-01-18 13:25:38 +0100

    wip

--- nlp/nlp_service.py
+++ nlp/nlp_service.py
@@ -124,6 +124,27 @@ def generate_lang_sentences_csv(lang):
         logger.error(f"Error while generating csv sentences datasets for langage {lang}: {e}", e)
 
 
+
+
+def generate_lang_list():
+    try:
+        filename = f"datasets/language/languages-{timezone.datetime.now().isoformat(sep='-',timespec='seconds')}.csv"
+        queryset = Constants.Langage.objects.filter(is_active=True)
+        dir_name = os.path.dirname(filename)
+        os.makedirs(dir_name, exist_ok=True)
+        with open(filename, 'w') as f:
+            writer = csv.writer(f, delimiter="\n")
+            writer.writerow(getattr(settings, Constants.LANG_CODES_FIELDS))
+            ## generate headers
+            for lang in queryset:
+                writer.writerow([lang.name, lang.code])
+
+            logger.info(f"csv list for langage codes generated in file {filename}")
+        
+    except Exception as e:
+        logger.error(f"Error while generating csv word list for langage {lang}: {e}", e)
+
+
 def generate_all_datasets():
     try:
         langages = Constants.Langage.objects.filter(is_active=True)
